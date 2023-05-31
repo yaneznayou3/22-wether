ed8420ec0f3b8a6c3cab6b62db5c4857ecafc619
yan3znausv01username <yaneznayou9gmail.com>
2023-05-31 10:00:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const apiKey = "e9a5d3b74bf84418b11193028231901";
+
+let form = document.querySelector("#Form");
+let inputForm = document.querySelector("#inputForm");
+let mainContainer = document.querySelector("main");
